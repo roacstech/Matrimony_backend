eae70f6e69e21dd60ad9f69ff3f3d2d3351e8cfe
roacs <roacstech@gmail.com>
2026-05-05 15:09:26 +0530

    Updated

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 const knex = require("knex");
 const knexConfig = require("../utils/knexFile");
 
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV || "OTDEV";
+
+console.log("Using DB Environment:", environment);
+
 const db = knex(knexConfig[environment]);
 
 db.raw("select 1+1 as result")
-  .then(() => console.log("Db Connected SuccessFully!😂"))
+  .then(() => console.log("Db Connected SuccessFully! 😂"))
   .catch(err => console.error("DB Connection Error:", err));
 
 module.exports = db;
